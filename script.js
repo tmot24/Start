f02b61cec4c6e9ed548661ce16fbe88510ef3e18
Grigory Yakovlev <grigory.yakovlev96@gmail.com>
2021-02-21 13:03:51 +0300

    homework 15

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
 
+if (numberOfFilms < 10) {
+    console.log("мало");
+} else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+    console.log("classic");
+} else if (numberOfFilms >= 30) {
+    console.log("киноман");
+} else {
+    console.log("error");
+}
+
 const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  private: false,
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false,
 };
 
-for (let i = 1; i <= 2; i++) {
-  const inputFilm = prompt("Один из последних просмотренных фильмов");
-  personalMovieDB.movies[inputFilm] = +prompt("На сколько оцените?");
+for (let i = 0; i < 2; i++) {
+    let inputFilm = prompt("Какой фильм вы посмотрели в последниий раз?");
+    if (inputFilm.length === 0 || inputFilm.length > 50 ){
+        inputFilm = prompt("Какой фильм вы посмотрели в последниий раз?");
+        i--;
+    } else {
+        personalMovieDB.movies[inputFilm] = +prompt("На сколько оцените?");
+    }
 }
 console.log(personalMovieDB);
